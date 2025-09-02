ecd2c5678ab1a541cb406eb53fb103943c067f6b
qwe <h@gmail.com>
2025-09-02 11:42:01 +0300

    qwertyuiop

--- src/git/yui.java
+++ src/git/yui.java
@@ -1,5 +1,12 @@
 package git;
-
+import java.util.Random;
 public class yui {
-
+	public static void main(String[]args) {
+		
+		Random rand = new Random();
+		int sk;
+		sk = rand.nextInt(1, 7);
+		System.out.println("--- "+sk+" ---");
+		
+	}
 }
